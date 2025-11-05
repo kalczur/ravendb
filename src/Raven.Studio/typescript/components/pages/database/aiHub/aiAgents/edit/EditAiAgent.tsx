1ffd354bd92d662ba8041d61482031b26531e9d0
Damian Olszewski <damian@ravendb.net>
2025-11-05 14:58:12 +0100

    RavenDB-25322 Use SplitView in EditAiAgent

--- src/Raven.Studio/typescript/components/pages/database/aiHub/aiAgents/edit/EditAiAgent.tsx
+++ src/Raven.Studio/typescript/components/pages/database/aiHub/aiAgents/edit/EditAiAgent.tsx
@@ -6,7 +6,6 @@ import EditAiAgentTestPanel from "./partials/EditAiAgentTestPanel";
 import { useAppSelector } from "components/store";
 import { editAiAgentSelectors } from "./store/editAiAgentSlice";
 import EditAiAgentInfoHub from "./partials/EditAiAgentInfoHub";
-import SizeGetter from "components/common/SizeGetter";
 import EditAiAgentBasicSection from "./partials/EditAiAgentBasicSection";
 import EditAiAgentParametersSection from "./partials/EditAiAgentParametersSection";
 import EditAiAgentToolsSection from "./partials/EditAiAgentToolsSection";
@@ -14,8 +13,7 @@ import EditAiAgentTrimmingSection from "./partials/EditAiAgentTrimmingSection";
 import { LoadingView } from "components/common/LoadingView";
 import { LoadError } from "components/common/LoadError";
 import useEditAiAgent from "./hooks/useEditAiAgent";
-import useResizableWidth from "components/hooks/useResizableWidth";
-import ColumnResize from "components/common/ColumnResize";
+import SplitView from "components/common/SplitView";
 
 interface QueryParams {
     id: string;
@@ -24,91 +22,52 @@ interface QueryParams {
 
 export default function EditAiAgent({ queryParams }: ReactQueryParamsProps<QueryParams>) {
     const aiAgentEditor = useEditAiAgent(queryParams);
+    const isTestOpen = useAppSelector(editAiAgentSelectors.isTestOpen);
+    const isTestPinned = useAppSelector(editAiAgentSelectors.isTestPinned);
 
     return (
         <FormProvider {...aiAgentEditor.editForm}>
             <form onSubmit={aiAgentEditor.handleSubmit} className="h-100 edit-ai-agent">
-                <SizeGetter render={({ width }) => <FormBody maxWidth={width} aiAgentEditor={aiAgentEditor} />} />
+                <SplitView isPanelOpen={isTestOpen} isPanelPinned={isTestPinned} minPanelWidth={300}>
+                    <SplitView.Body className="vstack">
+                        <div className="hstack justify-content-between align-items-start p-4">
+                            <AboutViewHeading
+                                title={`${aiAgentEditor.isEditAiAgent ? "Edit" : "Create"} AI Agent`}
+                                icon="ai-agents"
+                                marginBottom={0}
+                            />
+                            <EditAiAgentInfoHub />
+                        </div>
+                        <div className="px-4 pb-4 flex-grow-1 overflow-scroll h-100">
+                            {aiAgentEditor.asyncGetEditDefaultValues.loading && <LoadingView />}
+                            {aiAgentEditor.asyncGetEditDefaultValues.error && (
+                                <LoadError
+                                    error="Unable to load configuration"
+                                    refresh={aiAgentEditor.reloadEditForm}
+                                />
+                            )}
+                            {aiAgentEditor.asyncGetEditDefaultValues.result && (
+                                <>
+                                    <EditAiAgentBasicSection isEditAiAgent={aiAgentEditor.isEditAiAgent} />
+                                    <EditAiAgentParametersSection />
+                                    <EditAiAgentToolsSection />
+                                    <EditAiAgentTrimmingSection />
+                                </>
+                            )}
+                        </div>
+                        <div className="p-3 border-top border-secondary">
+                            <EditAiAgentFooter testForm={aiAgentEditor.testForm} editForm={aiAgentEditor.editForm} />
+                        </div>
+                    </SplitView.Body>
+                    <SplitView.Panel className="vstack panel-bg-1">
+                        <EditAiAgentTestPanel
+                            testForm={aiAgentEditor.testForm}
+                            editForm={aiAgentEditor.editForm}
+                            allQueriesNames={aiAgentEditor.allQueriesNames}
+                        />
+                    </SplitView.Panel>
+                </SplitView>
             </form>
         </FormProvider>
     );
 }
-
-interface FormBodyProps {
-    maxWidth: number;
-    aiAgentEditor: ReturnType<typeof useEditAiAgent>;
-}
-
-function FormBody({ maxWidth, aiAgentEditor }: FormBodyProps) {
-    const isTestOpen = useAppSelector(editAiAgentSelectors.isTestOpen);
-    const isTestPinned = useAppSelector(editAiAgentSelectors.isTestPinned);
-
-    const testAreaResizable = useResizableWidth({
-        initialWidth: maxWidth / 2,
-        minWidth: 300,
-        maxWidth: isTestPinned ? maxWidth - 400 : maxWidth - 50,
-    });
-
-    return (
-        <div className="hstack h-100 ai-agents-config" style={{ position: "relative" }}>
-            <div
-                className="vstack h-100"
-                style={{
-                    width: isTestOpen && isTestPinned ? `${maxWidth - testAreaResizable.width}px` : "100%",
-                }}
-            >
-                <div className="hstack justify-content-between align-items-start p-4">
-                    <AboutViewHeading
-                        title={`${aiAgentEditor.isEditAiAgent ? "Edit" : "Create"} AI Agent`}
-                        icon="ai-agents"
-                        marginBottom={0}
-                    />
-                    <EditAiAgentInfoHub />
-                </div>
-                <div className="px-4 pb-4 flex-grow-1 overflow-scroll h-100">
-                    {aiAgentEditor.asyncGetEditDefaultValues.loading && <LoadingView />}
-                    {aiAgentEditor.asyncGetEditDefaultValues.error && (
-                        <LoadError error="Unable to load configuration" refresh={aiAgentEditor.reloadEditForm} />
-                    )}
-                    {aiAgentEditor.asyncGetEditDefaultValues.result && (
-                        <>
-                            <EditAiAgentBasicSection isEditAiAgent={aiAgentEditor.isEditAiAgent} />
-                            <EditAiAgentParametersSection />
-                            <EditAiAgentToolsSection />
-                            <EditAiAgentTrimmingSection />
-                        </>
-                    )}
-                </div>
-                <div className="p-3 border-top border-secondary">
-                    <EditAiAgentFooter testForm={aiAgentEditor.testForm} editForm={aiAgentEditor.editForm} />
-                </div>
-            </div>
-            {isTestOpen && (
-                <div
-                    style={{
-                        width: `${testAreaResizable.width}px`,
-                        borderLeft: `1px solid ${testAreaResizable.isDragging ? "#ccc" : "#4c4c63"}`,
-                        zIndex: 12,
-                        position: "relative",
-                        ...(!isTestPinned && testPanelUnpinnedStyles),
-                    }}
-                    className="panel-bg-1 h-100 vstack"
-                >
-                    <ColumnResize handleMouseDown={testAreaResizable.handleMouseDown} />
-                    <EditAiAgentTestPanel
-                        testForm={aiAgentEditor.testForm}
-                        editForm={aiAgentEditor.editForm}
-                        allQueriesNames={aiAgentEditor.allQueriesNames}
-                    />
-                </div>
-            )}
-        </div>
-    );
-}
-
-const testPanelUnpinnedStyles: React.CSSProperties = {
-    position: "absolute",
-    right: 0,
-    top: 0,
-    bottom: 0,
-};
